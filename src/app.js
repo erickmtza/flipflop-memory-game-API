5aef24625378450c4a3230d98909d4781da23d20
Erick Martinez <erick.martinez2013@gmail.com>
2019-05-06 22:55:57 -0500

    Initial commit with API setup

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require('./config');
+
+const playersRouter = require('./players/players-router')
 
 const app = express()
 
@@ -12,12 +15,16 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 app.use(helmet())
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
+
+app.use('/api', playersRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
